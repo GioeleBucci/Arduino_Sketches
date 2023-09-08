fb150b74d8d55c20e47c47d2ec57b012a45e72ea
GioeleBucci <gioelebucci@gmail.com>
2023-09-08 08:57:01 +0200

    First commit

---
+++ Sketches/RGBLedTest/RGBLedTest.ino
@@ -0,0 +1,70 @@
+/*
+#define RED 3
+#define GREEN 5
+#define BLUE 6
+*/
+
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+#define DELAY delay(10)
+#define SLEEP_5_SEC delay(5000)
+
+void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+}
+
+void loop() {
+  // EFFECT 1 - black to white
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RED, i);
+    analogWrite(BLUE, i);
+    DELAY;
+  }
+
+
+  // EFFECT 2 - R-255 G-0↑ B-255↓
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(GREEN, i);
+    analogWrite(BLUE, 255 - i);
+    DELAY;
+  }
+
+
+  // EFFECT 3 - R-255↓ G-255 B-0↑
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RED, 255 - i);
+    analogWrite(BLUE, i);
+    DELAY;
+  }
+
+
+  // EFFECT 4 - R-0↑ G-255↓ B-255
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RED, i);
+    analogWrite(GREEN, 255 - i);
+    DELAY;
+  }
+
+
+  // DIM TO BLACK
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(BLUE, 255 - i);
+    analogWrite(RED, 255 - i);
+    DELAY;
+  }
+
+  SLEEP_5_SEC;
+
+}
---
+++ Sketches/SingleLedTest/SingleLedTest.ino
@@ -0,0 +1,24 @@
+#define DELAY_TIME 10  // fading time between colors
+#define LED_PIN 5      // use pin 5 -> ~5V
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);  // use pin 5 -> ~5V
+  digitalWrite(LED_PIN, LOW);
+}
+
+// main loop
+void loop() {
+  // gradually turn on
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(LED_PIN, i);
+    delay(DELAY_TIME);
+  }
+
+  // gradually turn off
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(LED_PIN, i);
+    delay(DELAY_TIME);
+  }
+
+  delay(3000);
+}
